a6a8a7e7357d809c6e81603548df5116aa57ea7b
bhaktiramadhanii <bakti0048@gmail.com>
2022-09-22 20:49:12 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Link to="/login">login</Link>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
